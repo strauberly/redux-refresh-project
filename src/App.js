5c627f7cb434e6341855cb03a57005dfc931d9c6
Brian Crookshanks <zeron30@hotmail.com>
2022-10-10 07:46:52 -0500

    redux and useEffect()

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  async function submitCart() {
+    await supabase
+      .from({ cart })
+      .insert([{ items: Cart.cartItems, : cartCtx.items }]);
+  }
+  useEffect(() => {
+    submitCart();
+  });
   return (
     <Layout>
       {showCart && <Cart />}
--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -6,6 +6,7 @@ import classes from "./ProductItem.module.css";
 const ProductItem = (props) => {
   const dispatch = useDispatch();
   const { title, price, description, id } = props;
+
   const addToCartHandler = () => {
     dispatch(
       cartActions.addItemToCart({
