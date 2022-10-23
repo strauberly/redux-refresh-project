4d2c8955846c874f8c06a04bcd215f0e5523f599
Brian Crookshanks <zeron30@hotmail.com>
2022-10-22 21:57:27 -0500

    return formatting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Fragment, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import Notification from "./components/UI/Notification";
-import { sendCartData, getCartData } from "./store/cart-actions";
+import { sendCartData, fetchCartData } from "./store/cart-actions";
 
 let isInitial = true;
 
@@ -15,7 +16,7 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    dispatch(getCartData());
+    dispatch(fetchCartData());
   }, [dispatch]);
 
   useEffect(() => {
@@ -45,3 +46,53 @@ function App() {
 }
 
 export default App;
+
+// import { Fragment, useEffect } from "react";
+// import { useSelector, useDispatch } from "react-redux";
+// import Cart from "./components/Cart/Cart";
+// import Layout from "./components/Layout/Layout";
+// import Products from "./components/Shop/Products";
+// import Notification from "./components/UI/Notification";
+// import { sendCartData, getCartData } from "./store/cart-actions";
+
+// let isInitial = true;
+
+// function App() {
+//   const dispatch = useDispatch();
+//   const showCart = useSelector((state) => state.ui.cartIsVisible);
+//   const cart = useSelector((state) => state.cart);
+//   const notification = useSelector((state) => state.ui.notification);
+
+//   useEffect(() => {
+//     dispatch(getCartData());
+//   }, [dispatch]);
+
+//   useEffect(() => {
+//     if (isInitial) {
+//       isInitial = false;
+//       return;
+//     }
+
+//     if (cart.changed) {
+//       dispatch(sendCartData(cart));
+//     }
+//   }, [cart, dispatch]);
+
+//   return (
+//     <Fragment>
+//       {notification && (
+//         <Notification
+//           status={notification.status}
+//           title={notification.title}
+//           message={notification.message}
+//         />
+//       )}
+//       <Layout>
+//         {showCart && <Cart />}
+//         <Products />
+//       </Layout>
+//     </Fragment>
+//   );
+// }
+
+// export default App;
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,5 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+// import { uiActions } from "./ui-slice";
+
+// import { createClient } from "@supabase/supabase-js";
+
+// const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+// const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+// const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
 const cartSlice = createSlice({
   name: "cart",
   initialState: {
@@ -18,10 +26,10 @@ const cartSlice = createSlice({
       if (!existingItem) {
         state.items.push({
           id: newItem.id,
-          name: newItem.title,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
+          name: newItem.title,
         });
       } else {
         existingItem.quantity++;
@@ -36,12 +44,211 @@ const cartSlice = createSlice({
         state.items = state.items.filter((item) => item.id !== id);
       } else {
         existingItem.quantity--;
-        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
       }
     },
   },
 });
 
+// export const sendCartData = (cart) => {
+//   return async (dispatch) => {
+//     dispatch(
+//       uiActions.showNotification({
+//         status: "pending",
+//         title: "Sending...",
+//         message: "Sending cart data!",
+//       })
+//     );
+//     const sendRequest = async () => {
+//       //       const response = await supabase
+//       //         .from("cartContents")
+//       //         .insert({ items: cart, totalQuantity: cart.totalQuantity });
+
+//       //       if (response.status !== 201) {
+//       //         throw new Error("Sending cart data failed.");
+//       //       }
+//       //     };
+
+//       //     try {
+//       //       await sendRequest();
+//       // const sendCart = async () =>
+//       // await supabase.from("cartContents").insert({ cartContents: cart });
+
+//       const response = await supabase
+//         .from("cartContents")
+//         .insert({ cartContent: cart });
+
+//       if (!response.ok) {
+//         throw new Error("Sending cart data failed.");
+//       } else {
+//         await supabase.from("cartContents").insert({ cart });
+//       }
+//     };
+
+//     try {
+//       await sendRequest();
+
+//       dispatch(
+//         uiActions.showNotification({
+//           status: "success",
+//           title: "Success!",
+//           message: "Sent cart data successfully!",
+//         })
+//       );
+//     } catch (error) {
+//       dispatch(
+//         uiActions.showNotification({
+//           status: "error",
+//           title: "Error!",
+//           message: "Sending cart data failed!",
+//         })
+//       );
+//     }
+//   };
+// };
+
 export const cartActions = cartSlice.actions;
 
 export default cartSlice;
+
+// import { createSlice } from "@reduxjs/toolkit";
+
+// import { uiActions } from "./ui-slice";
+
+// const cartSlice = createSlice({
+//   name: "cart",
+//   initialState: {
+//     items: [],
+//     totalQuantity: 0,
+//   },
+//   reducers: {
+//     replaceCart(state, action) {
+//       state.totalQuantity = action.payload.totalQuantity;
+//       state.items = action.payload.items;
+//     },
+//     addItemToCart(state, action) {
+//       const newItem = action.payload;
+//       const existingItem = state.items.find((item) => item.id === newItem.id);
+//       state.totalQuantity++;
+//       if (!existingItem) {
+//         state.items.push({
+//           id: newItem.id,
+//           price: newItem.price,
+//           quantity: 1,
+//           totalPrice: newItem.price,
+//           name: newItem.title,
+//         });
+//       } else {
+//         existingItem.quantity++;
+//         existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+//       }
+//     },
+//     removeItemFromCart(state, action) {
+//       const id = action.payload;
+//       const existingItem = state.items.find((item) => item.id === id);
+//       state.totalQuantity--;
+//       if (existingItem.quantity === 1) {
+//         state.items = state.items.filter((item) => item.id !== id);
+//       } else {
+//         existingItem.quantity--;
+//       }
+//     },
+//   },
+// });
+
+// export const sendCartData = (cart) => {
+//   return async (dispatch) => {
+//     dispatch(
+//       uiActions.showNotification({
+//         status: "pending",
+//         title: "Sending...",
+//         message: "Sending cart data!",
+//       })
+//     );
+
+//     const sendRequest = async () => {
+//       const response = await fetch(
+//         "https://react-http-6b4a6.firebaseio.com/cart.json",
+//         {
+//           method: "PUT",
+//           body: JSON.stringify(cart),
+//         }
+//       );
+
+//       if (!response.ok) {
+//         throw new Error("Sending cart data failed.");
+//       }
+//     };
+
+//     try {
+//       await sendRequest();
+
+//       dispatch(
+//         uiActions.showNotification({
+//           status: "success",
+//           title: "Success!",
+//           message: "Sent cart data successfully!",
+//         })
+//       );
+//     } catch (error) {
+//       dispatch(
+//         uiActions.showNotification({
+//           status: "error",
+//           title: "Error!",
+//           message: "Sending cart data failed!",
+//         })
+//       );
+//     }
+//   };
+// };
+
+// export const cartActions = cartSlice.actions;
+
+// export default cartSlice;
+
+// // import { createSlice } from "@reduxjs/toolkit";
+
+// // const cartSlice = createSlice({
+// //   name: "cart",
+// //   initialState: {
+// //     items: [],
+// //     totalQuantity: 0,
+// //   },
+// //   reducers: {
+// //     replaceCart(state, action) {
+// //       state.totalQuantity = action.payload.totalQuantity;
+// //       state.items = action.payload.items;
+// //     },
+// //     addItemToCart(state, action) {
+// //       const newItem = action.payload;
+// //       const existingItem = state.items.find((item) => item.id === newItem.id);
+// //       state.totalQuantity++;
+// //       if (!existingItem) {
+// //         state.items.push({
+// //           id: newItem.id,
+// //           name: newItem.title,
+// //           price: newItem.price,
+// //           quantity: 1,
+// //           totalPrice: newItem.price,
+// //         });
+// //       } else {
+// //         existingItem.quantity++;
+// //         existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+// //       }
+// //     },
+// //     removeItemFromCart(state, action) {
+// //       const id = action.payload;
+// //       const existingItem = state.items.find((item) => item.id === id);
+// //       state.totalQuantity--;
+// //       if (existingItem.quantity === 1) {
+// //         state.items = state.items.filter((item) => item.id !== id);
+// //       } else {
+// //         existingItem.quantity--;
+// //         existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+// //       }
+// //     },
+// //   },
+// // });
+
+// // export const cartActions = cartSlice.actions;
+
+// // export default cartSlice;
