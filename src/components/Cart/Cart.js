2730e0c49c070debced4e993a3148564ac4592f6
Brian Crookshanks <zeron30@hotmail.com>
2022-10-24 16:58:19 -0500

    cart data persists

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -29,34 +29,3 @@ const Cart = (props) => {
 };
 
 export default Cart;
-
-// import { useSelector } from "react-redux";
-// import Card from "../UI/Card";
-// import classes from "./Cart.module.css";
-// import CartItem from "./CartItem";
-
-// const Cart = (props) => {
-//   const cartItems = useSelector((state) => state.cart.items);
-//   console.log(cartItems);
-//   return (
-//     <Card className={classes.cart}>
-//       <h2>Your Shopping Cart</h2>
-//       <ul>
-//         {cartItems.map((item) => (
-//           <CartItem
-//             key={item.id}
-//             item={{
-//               id: item.id,
-//               title: item.name,
-//               price: item.price,
-//               quantity: item.quantity,
-//               total: item.totalPrice,
-//             }}
-//           />
-//         ))}
-//       </ul>
-//     </Card>
-//   );
-// };
-
-// export default Cart;
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,28 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-// import { uiActions } from "./ui-slice";
-
-// import { createClient } from "@supabase/supabase-js";
-
-// const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-// const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
-// const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
 const cartSlice = createSlice({
   name: "cart",
   initialState: {
     items: [],
     totalQuantity: 0,
+    changed: false,
   },
   reducers: {
     replaceCart(state, action) {
       state.totalQuantity = action.payload.totalQuantity;
       state.items = action.payload.items;
+      console.log(state.totalQuantity);
+      console.log(state.items);
     },
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
+      state.changed = true;
       if (!existingItem) {
         state.items.push({
           id: newItem.id,
@@ -40,215 +36,16 @@ const cartSlice = createSlice({
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
       state.totalQuantity--;
+      state.changed = true;
       if (existingItem.quantity === 1) {
         state.items = state.items.filter((item) => item.id !== id);
       } else {
         existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
       }
     },
   },
 });
 
-// export const sendCartData = (cart) => {
-//   return async (dispatch) => {
-//     dispatch(
-//       uiActions.showNotification({
-//         status: "pending",
-//         title: "Sending...",
-//         message: "Sending cart data!",
-//       })
-//     );
-//     const sendRequest = async () => {
-//       //       const response = await supabase
-//       //         .from("cartContents")
-//       //         .insert({ items: cart, totalQuantity: cart.totalQuantity });
-
-//       //       if (response.status !== 201) {
-//       //         throw new Error("Sending cart data failed.");
-//       //       }
-//       //     };
-
-//       //     try {
-//       //       await sendRequest();
-//       // const sendCart = async () =>
-//       // await supabase.from("cartContents").insert({ cartContents: cart });
-
-//       const response = await supabase
-//         .from("cartContents")
-//         .insert({ cartContent: cart });
-
-//       if (!response.ok) {
-//         throw new Error("Sending cart data failed.");
-//       } else {
-//         await supabase.from("cartContents").insert({ cart });
-//       }
-//     };
-
-//     try {
-//       await sendRequest();
-
-//       dispatch(
-//         uiActions.showNotification({
-//           status: "success",
-//           title: "Success!",
-//           message: "Sent cart data successfully!",
-//         })
-//       );
-//     } catch (error) {
-//       dispatch(
-//         uiActions.showNotification({
-//           status: "error",
-//           title: "Error!",
-//           message: "Sending cart data failed!",
-//         })
-//       );
-//     }
-//   };
-// };
-
 export const cartActions = cartSlice.actions;
-
 export default cartSlice;
-
-// import { createSlice } from "@reduxjs/toolkit";
-
-// import { uiActions } from "./ui-slice";
-
-// const cartSlice = createSlice({
-//   name: "cart",
-//   initialState: {
-//     items: [],
-//     totalQuantity: 0,
-//   },
-//   reducers: {
-//     replaceCart(state, action) {
-//       state.totalQuantity = action.payload.totalQuantity;
-//       state.items = action.payload.items;
-//     },
-//     addItemToCart(state, action) {
-//       const newItem = action.payload;
-//       const existingItem = state.items.find((item) => item.id === newItem.id);
-//       state.totalQuantity++;
-//       if (!existingItem) {
-//         state.items.push({
-//           id: newItem.id,
-//           price: newItem.price,
-//           quantity: 1,
-//           totalPrice: newItem.price,
-//           name: newItem.title,
-//         });
-//       } else {
-//         existingItem.quantity++;
-//         existingItem.totalPrice = existingItem.totalPrice + newItem.price;
-//       }
-//     },
-//     removeItemFromCart(state, action) {
-//       const id = action.payload;
-//       const existingItem = state.items.find((item) => item.id === id);
-//       state.totalQuantity--;
-//       if (existingItem.quantity === 1) {
-//         state.items = state.items.filter((item) => item.id !== id);
-//       } else {
-//         existingItem.quantity--;
-//       }
-//     },
-//   },
-// });
-
-// export const sendCartData = (cart) => {
-//   return async (dispatch) => {
-//     dispatch(
-//       uiActions.showNotification({
-//         status: "pending",
-//         title: "Sending...",
-//         message: "Sending cart data!",
-//       })
-//     );
-
-//     const sendRequest = async () => {
-//       const response = await fetch(
-//         "https://react-http-6b4a6.firebaseio.com/cart.json",
-//         {
-//           method: "PUT",
-//           body: JSON.stringify(cart),
-//         }
-//       );
-
-//       if (!response.ok) {
-//         throw new Error("Sending cart data failed.");
-//       }
-//     };
-
-//     try {
-//       await sendRequest();
-
-//       dispatch(
-//         uiActions.showNotification({
-//           status: "success",
-//           title: "Success!",
-//           message: "Sent cart data successfully!",
-//         })
-//       );
-//     } catch (error) {
-//       dispatch(
-//         uiActions.showNotification({
-//           status: "error",
-//           title: "Error!",
-//           message: "Sending cart data failed!",
-//         })
-//       );
-//     }
-//   };
-// };
-
-// export const cartActions = cartSlice.actions;
-
-// export default cartSlice;
-
-// // import { createSlice } from "@reduxjs/toolkit";
-
-// // const cartSlice = createSlice({
-// //   name: "cart",
-// //   initialState: {
-// //     items: [],
-// //     totalQuantity: 0,
-// //   },
-// //   reducers: {
-// //     replaceCart(state, action) {
-// //       state.totalQuantity = action.payload.totalQuantity;
-// //       state.items = action.payload.items;
-// //     },
-// //     addItemToCart(state, action) {
-// //       const newItem = action.payload;
-// //       const existingItem = state.items.find((item) => item.id === newItem.id);
-// //       state.totalQuantity++;
-// //       if (!existingItem) {
-// //         state.items.push({
-// //           id: newItem.id,
-// //           name: newItem.title,
-// //           price: newItem.price,
-// //           quantity: 1,
-// //           totalPrice: newItem.price,
-// //         });
-// //       } else {
-// //         existingItem.quantity++;
-// //         existingItem.totalPrice = existingItem.totalPrice + newItem.price;
-// //       }
-// //     },
-// //     removeItemFromCart(state, action) {
-// //       const id = action.payload;
-// //       const existingItem = state.items.find((item) => item.id === id);
-// //       state.totalQuantity--;
-// //       if (existingItem.quantity === 1) {
-// //         state.items = state.items.filter((item) => item.id !== id);
-// //       } else {
-// //         existingItem.quantity--;
-// //         existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
-// //       }
-// //     },
-// //   },
-// // });
-
-// // export const cartActions = cartSlice.actions;
-
-// // export default cartSlice;
